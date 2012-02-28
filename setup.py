920d9e5c40631610b4c4e296222df365e824b686
Thomas Hunger <tehunger@gmail.com>
2012-02-28 09:51:20 +0000

    Stub empty sketch library.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sketch',
+    version='0.1',
+    license='BSD',
+    author='Thomas Huger',
+    author_email='tehunger@gmail.com',
+    description='Tools to build statistical sketches.',
+    packages=['sketch'],
+)

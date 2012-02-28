4604d81f343352a246b6b293e04c4d868073152e
Thomas Hunger <tehunger@gmail.com>
2012-02-28 09:58:32 +0000

    Import Bob Jenkin's lookup3 hash.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 setup(
     name='sketch',
@@ -8,4 +10,7 @@ setup(
     author_email='tehunger@gmail.com',
     description='Tools to build statistical sketches.',
     packages=['sketch'],
+
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("_sketch", ['sketch/lookup3.c', 'sketch/sketch.pyx'])],
 )

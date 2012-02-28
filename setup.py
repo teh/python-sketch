1d7fe09bc92f2f85faabf3303d6377d1d30992d5
Thomas Hunger <tehunger@gmail.com>
2012-02-28 11:48:45 +0000

    Typo. Thanks Dan :)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='sketch',
     version='0.1',
     license='BSD',
-    author='Thomas Huger',
+    author='Thomas Hunger',
     author_email='tehunger@gmail.com',
     description='Tools to build statistical sketches.',
     packages=['sketch'],
